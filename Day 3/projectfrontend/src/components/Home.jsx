50b972df141675e3524f57ddf3c53a98c72c2255
NithinM754 <110536349+NithinM754@users.noreply.github.com>
2023-09-15 10:31:56 +0530

    Add files via upload

---
+++ Day 3/projectfrontend/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export const Home=()=>{
+    return (
+        <>
+<div>
+    <p>
+        
+        <h1>Home Page</h1>
+    </p>
+</div> 
+        <div>
+<p>
+
+        <h2>Student Information management System</h2>
+</p>
+        </div>
+        </>
+        )
+    }
