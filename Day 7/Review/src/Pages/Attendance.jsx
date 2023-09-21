336da32cc8605458681a5cbd7e71411551596aa8
NithinM754 <110536349+NithinM754@users.noreply.github.com>
2023-09-21 15:10:56 +0530

    Add files via upload

---
+++ Day 7/Review/src/Pages/Attendance.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from '../Components/Layout'
+
+function Attendance() {
+  return (
+    <>
+
+            <div className='main-wrapper'>
+                <div className='welcome-container'>
+                  
+                    <h1> Welcome to our Attendance Page </h1>
+                </div>
+            </div>
+        </>
+  )
+}
+
+export default Attendance;
